ded3ea905b4b365747d6d89afddcb6dfc43ba712
Cameron Lares <cameronlares@gmail.com>
2020-07-17 15:02:16 -0400

    New Commit

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import smurf from '/Users/cameronlares/Desktop/LambdaAssignments/Unit_3/Week_2/web-sprint-challenge-advanced-state-management/server.js'
 export const FETCH_SMURF_START = 'FETCH_SMURF_START'
 export const FETCH_SMURF_SUCESS = 'FETCH_SMURF_SUCESS'
 export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE'
@@ -9,36 +8,50 @@ export const ADD_SMURF = 'ADD_SMURF'
 
 export const fetchSmurfs = () => {
     return dispatch => {
-                //1st dispatch an isLoading action
+        //1st dispatch an isLoading action
 
-dispatch({ type: FETCH_SMURF_START})
+        dispatch({
+            type: FETCH_SMURF_START
+        })
         //2nd - do the async operation
 
         axios
-        .get("http://localhost:3333/smurfs")
-        .then(res=> {
-            dispatch({ type:FETCH_SMURF_SUCESS, payload: res.data}) //fetching smurfs
-        })
-        .catch(err =>{
-            dispatch({ type: FETCH_SMURF_FAILURE, payload: err.message})
-        })
-//ADD SMURF
+            .get("http://localhost:3333/smurfs")
+            .then(res => {
+                dispatch({
+                    type: FETCH_SMURF_SUCESS,
+                    payload: res.data
+                }) //fetching smurfs
+            })
+            .catch(err => {
+                dispatch({
+                    type: FETCH_SMURF_FAILURE,
+                    payload: err.message
+                })
+            })
+        //ADD SMURF
 
 
     }
 }
 
-export const addSmurf = (newSmurf) => {
-    return dispatch => {
-axios
-.post('http://localhost:3333/smurfs', {
-    dispatch ({type: ADD_SMURF, payload:newSmurf})
-})
-.then(res=>{
-    console.log(res.data)
-})
-.catch(err =>{
-    dispatch({ type: FETCH_SMURF_FAILURE, payload: err.message})
-})
-    }
+export const addSmurf = newSmurf => dispatch => {
+
+    axios.post('http://localhost:3333/smurfs', 
+
+            dispatch({
+                type: ADD_SMURF,
+                payload: newSmurf
+            })
+        )
+
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            dispatch({
+                type: FETCH_SMURF_FAILURE,
+                payload: err.message
+            });
+        })
 }
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,6 +5,19 @@ import App from "./components/App";
 import {Provider} from "react-redux"
 import logger from 'redux-logger'
 import {createStore,applyMiddleware} from 'redux'
-// import thunk from "redux-thunk"
+import thunk from "redux-thunk"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {reducer} from "./reducers/reducer"
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Provider store = {store}>
+      <App />
+      </Provider>
+    
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+
